608f34ef0070a89b8d912201556d6cdcfeb6cec7
adnan-sajjad <adnansajjadadnan186@gmail.com>
2026-07-21 00:08:30 +0530

    correct intro

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -96,7 +96,7 @@ const HeroSection = () => {
         preload="auto"
         className="absolute inset-0 h-full w-full object-cover"
       >
-        <source src="/avatarintro.mp4" type="video/mp4" />
+        <source src="/adintro1.mp4" type="video/mp4" />
       </video>
 
       {/* Cinematic gradient overlays */}
